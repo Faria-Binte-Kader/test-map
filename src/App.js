ec5ae9cb3b602bb4e2416e38698dbcf8cbe6d98b
Faria-Binte-Kader <fariabintekader67@gmail.com>
2022-04-19 11:51:01 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import * as parkDate from "./data/Skateboard_Parks.geojson";
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-function App() {
+export default function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactMapGL
+        initialViewState={{
+          latitude:  23.7771761,
+          longitude: 90.399452,
+          zoom: 14
+        }}
+        style={{width: 600, height: 400}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken='pk.eyJ1IjoiZmFyaWFiaW50ZWthZGVyIiwiYSI6ImNsMjVwMWdyNDA2YmozYm8wZDk1MDkyb2sifQ.MNgRzV6q5svRlvzeziFZsQ'
+      >
+         <Marker longitude={-75.6903} latitude={45.4211} color="red" />
+      </ReactMapGL>
     </div>
   );
 }
-
-export default App;
